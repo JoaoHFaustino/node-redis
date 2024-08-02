b523771994990514f554fbddf39ab9317a953a59
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-02 00:30:34 -0300

    feat: implementing usecase and sending the data to redis

--- src/data/usecases/products/addProduct.ts
+++ src/data/usecases/products/addProduct.ts
@@ -1,8 +1,13 @@
 import { IAddProductProtocol } from "@/domain/protocols/products";
+import redisClient from "@/infra/cache/redisClient";
 
 export class AddProduct implements IAddProductProtocol {
-    addProduct(params: IAddProductProtocol.Params): Promise<IAddProductProtocol.Result> {
-        throw new Error("Method not implemented.");
+    async addProduct(params: IAddProductProtocol.Params): Promise<IAddProductProtocol.Result> {
+        try {
+            await redisClient.set(`product:${params.id}`, JSON.stringify(params), 7200);
+            return { success: true, message: 'Product added successfully' };
+        } catch (error) {
+            return { success: false, message: 'Failed to add product' };
+        }
     }
-
-} 
+}
