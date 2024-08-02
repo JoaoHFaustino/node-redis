f21db3fcfe78da9aca2d17d1cd18e586c7d15503
João Henrique Faustino <40185240+Joaohenrique15@users.noreply.github.com>
2024-08-02 00:45:37 -0300

    feat: implementing controllers and routes

---
+++ src/data/usecases/products/index.ts
@@ -0,0 +1,4 @@
+export * from './addProduct'
+export * from './getProducts'
+export * from './getProductById'
+export * from './deleteProduct'
--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { ProductsController } from "@/controllers/products/productsController";
 
 const productsRoutes = Router();
+const productsController = new ProductsController();
 
-productsRoutes.post('/products')
-productsRoutes.get('/products')
-productsRoutes.get('/products/:id')
-productsRoutes.delete('/products')
+productsRoutes.post('/products', (req, res) => productsController.createProduct(req, res));
+productsRoutes.get('/products', (req, res) => productsController.getProducts(req, res));
+productsRoutes.get('/products/:id', (req, res) => productsController.getProductById(req, res));
+productsRoutes.delete('/products/:id',(req, res) => productsController.deleteProduct(req, res));
 
 export default productsRoutes
